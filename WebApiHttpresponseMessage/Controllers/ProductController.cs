09790a093bb5a23ba33475b6bcef8432a78ac0f6
kasi <kasinayana2@gmail.com>
2022-11-24 11:37:36 +0530

    Add project files.

---
+++ WebApiHttpresponseMessage/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiHttpresponseMessage.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public string[] GetAll()
+        {
+            string[] products = { "Tv", "Mobile", "Fridge", "Bike", "Laptop" };
+            return products;
+        }
+        public string Getprodct(int id)
+        {
+            string[] products = { "Tv", "Mobile", "Fridge", "Bike", "Laptop" };
+            if(id>=0 && id <= 4)
+            {
+                return products[id];
+            }
+            else
+            {
+                return "NotFound";
+            }
+        }
+    }
+}
---
+++ WebApiHttpresponseMessage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiHttpresponseMessage.WebApiApplication" Language="C#" %>
